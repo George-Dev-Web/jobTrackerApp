e8aeb9b49da6c04b827cf5bf9e5db27dc7abf1af
George <georgemwazuna3@gmail.com>
2025-04-25 11:03:23 +0300

    First CSS iteration

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import { useEffect, useState } from 'react'
-import Login from './pages/Login';
+import { useEffect, useState } from "react";
+import Login from "./pages/Login";
 
 const apiKey = import.meta.env.VITE_API_KEY;
 
-
 const API_URL = "https://www.themuse.com/api/public/jobs?page=1";
 function App() {
-  const [jobs, setJobs] = useState([])
-  useEffect(()=>{
+  const [jobs, setJobs] = useState([]);
+  useEffect(() => {
     fetch(API_URL)
-    .then(r => r.json())
-    .then(data => setJobs(data.results))
-  },[])
+      .then((r) => r.json())
+      .then((data) => setJobs(data.results));
+  }, []);
   return (
     <>
       <h1>Home page</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
