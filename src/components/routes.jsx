1abc0fde65445ecc259dd7a00396cd90cccb91ce
George <georgemwazuna3@gmail.com>
2025-04-25 10:39:23 +0300

    First commit

--- src/components/routes.jsx
+++ src/components/routes.jsx
@@ -1,50 +1,39 @@
 import App from "../App";
 import ErrorPage from "../pages/ErrorPage";
-import JobBoard from "../pages/JobBard/JobBoard";
+import JobBoard from "../pages/JobBoard/JobBoard";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import ProtectedRoutes from "./ProtectedRoutes";
 
-
-export const routes =[ 
-    {
+export const routes = [
+  {
+    path: "/",
+    element: <ProtectedRoutes />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
         path: "/",
-        element: <ProtectedRoutes/>,
-        errorElement: <ErrorPage />,
-        children: [
-            {
-                path: "/",
-                element: <App/>,
-                children: [
+        element: <App />,
+        children: [],
+      },
+      {
+        path: "/job-listing",
+        element: <JobBoard />,
+        children: [],
+      },
+    ],
+  },
 
-                ]
-            },
-            {
-                path: "/job-listing",
-                element: <JobBoard/>,
-                children: [
-        
-                ]
-            },
-            
-        ]
-    },
-    
-    {
-        path: "/login",
-        element: <Login/>,
-        errorElement: <ErrorPage />,
-        children: [
-
-        ]
-    },
-    {
-        path: "/register",
-        element: <Register/>,
-        errorElement: <ErrorPage />,
-        children: [
-
-        ]
-    },
-
-]
+  {
+    path: "/login",
+    element: <Login />,
+    errorElement: <ErrorPage />,
+    children: [],
+  },
+  {
+    path: "/register",
+    element: <Register />,
+    errorElement: <ErrorPage />,
+    children: [],
+  },
+];
